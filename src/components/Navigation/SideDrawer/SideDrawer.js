4ce72add5e48c54a2572073d1b39bff92a187463
roy <roystern92@gmail.com>
2020-01-12 16:04:09 +0200

    Updated Sidedrawer component

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,5 +1,7 @@
-import React, { Fragment } from 'react';
+import React, {Fragment} from 'react';
 import Logo from '../../Logo/Logo';
+import {NavLink} from 'react-router-dom';
+
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.module.css';
 import Backdrop from '../../UI/Backdrop/Backdrop';
@@ -9,16 +11,79 @@ const sideDrawer = (props) => {
     if (props.open) {
         attachedClasses = [classes.SideDrawer, classes.Open];
     }
+
+    let routes = <nav>
+        <NavLink
+            exact
+            to='/signIn'
+        >
+            <div className={classes.MobileNavItem}>
+                Sign in
+            </div>
+
+        </NavLink>
+
+        <NavLink
+            exact
+            to='/signUp'
+        >
+            <div className={classes.MobileNavItem}>
+                Sign up
+            </div>
+
+        </NavLink>
+
+    </nav>;
+
+
+    if(props.isAuth){
+        routes =  <nav>
+            <NavLink
+                exact
+                to='/my-day'
+            >
+                <div className={classes.MobileNavItem}>
+                    My day
+                </div>
+
+            </NavLink>
+
+            <NavLink
+                exact
+                to='/profile'
+            >
+                <div className={classes.MobileNavItem}>
+                    Profile
+                </div>
+
+            </NavLink>
+
+
+            <NavLink
+                exact
+                to='/logout'
+            >
+                <div className={classes.MobileNavItem}>
+                    Log out
+                </div>
+
+            </NavLink>
+
+        </nav>;
+
+    }
+
+
     return (
         <Fragment>
-            <Backdrop show={props.open} clicked={props.closed} />
+            <Backdrop show={props.open} clicked={props.closed}/>
             <div className={attachedClasses.join(' ')} onClick={props.closed}>
                 <div className={classes.Logo}>
-                    <Logo />
+                    <Logo/>
                 </div>
-                <nav>
-                    <NavigationItems isAuth={props.isAuth} />
-                </nav>
+
+                {routes}
+
             </div>
         </Fragment>
     );
